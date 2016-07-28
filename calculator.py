45a6912490691e21f3364f0eef33f03cf310f282
Intro Student <you@example.com>
2016-07-27 21:11:25 -0700

    first commit

---
+++ calculator.py
@@ -0,0 +1,125 @@
+#FUNCTIONS
+#EXTERCISE1
+# def hello_print():
+# 	print "hello"
+# hello_print()
+
+# def hello_return():
+# 	return "hello"
+# greeting = hello_return()
+# print greeting
+
+# def hello_name(name):
+# 	print "hello", name
+
+# hello_name("Liz")
+
+# partner_name= "Kathy"
+# print hello_name(partner_name)
+
+#EXERCISE 2
+
+def add(num1, num2):
+	return num1 + num2
+print add(4,5)
+
+def sub(num1, num2):
+	return num1 - num2
+print sub(10, 12)
+
+def multiply(num1, num2):
+	return num1 * num2
+print multiply(2, 3) 
+
+def divide(num1, num2):
+	return num1 / num2
+print divide (4, 2)
+
+def modulo(num1, num2):
+	return num1 % num2
+print modulo(4, 2)
+
+def power (base, exponent):
+	return base**exponent
+print power (5, 2)
+
+def square (num):
+	return num**2
+print square(4)
+
+
+#a
+print add(add(4, 5), sub(9,6))
+
+#b
+print sub(divide(12, 2),(60))
+
+#c
+print add(add(1,2), 3)
+
+#d
+print power(add (1,2), 2)
+
+#e
+print divide(modulo(3,4), multiply(9,9))
+
+#f
+print multiply(7, add( 3, 8))
+
+#g
+print sub(add(1,2),multiply(3, add(4,5)))
+
+#h
+print power(3, add ( 2, 3))
+
+
+
+
+
+
+
+
+
+# def a(n1,n2,n3,n4):
+# 	suma = n1 + n2
+# 	difference = n3 - n4
+# 	total = sum + difference
+# 	return total 
+# print a(4,5,9,6) 
+
+# def b(num1, num2, num3):
+# 	quotient = num1 / num2
+# 	total = quotient - num3
+# 	return total
+# print b(12, 2, 60)	
+
+# def c(n1, n2, n3):
+# 	sum = n1 + n2 + n3
+# 	return sum
+# print c(1,2,3)
+
+# def d( num1, num2, num3):
+# 	sumd = num1 + num2
+# 	total = sum ** num3
+# 	return total
+# print d(1, 2, 2) 
+
+# def e(n1,n2,n3,n4):
+# 	modulo = n1 % n2
+# 	product = n3 * n4 
+# 	total = modulo/product
+# 	return total
+# print e(3,4,9,9)
+
+# def f(n1, n2, n3):
+# 	sum = n2 + n3
+# 	total= n1 * sum
+# 	return total
+# print f(7, 3, 8)
+
+
+
+
+
+
+
